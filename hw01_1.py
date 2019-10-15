0176f2082732fc9ec2b906e8a5c85d634062d0cc
ashabalin35 <ashabalin35@gmail.com>
2019-10-15 16:52:50 +0300

    Задание 1 выполнено

--- hw01_1.py
+++ hw01_1.py
@@ -5,8 +5,12 @@ user_name = input('Введите имя пользователя GITHUB: ')
 gets = requests.get(main_link+user_name+'/repos')
 if gets.ok:
     data = json.loads(gets.text)
-    print(f'Список всех доступных репозиториев пользователя {user_name}:\n')
+    with open('github.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+        print('Cохранили полученные данные в файл github.json\n')
+    print(f'Смотрим список всех доступных репозиториев пользователя {user_name}:\n')
     for i in data:
         print(i['name'])
+
 else:
     print('Неверное имя пользователя')
